ed82f0057b3b6481797c8caca1cbce8751c5ba82
Shandy-ko <shandy1124gaff@gmail.com>
2015-11-15 22:17:49 +0900

    test.py add Thread and Context function

--- my_test.py
+++ my_test.py
@@ -11,5 +11,22 @@ debugger = my_debugger.debugger()
 pid = raw_input("Enter the PID of the process to atach to: ")
 
 debugger.attach(int(pid))
-debugger.run()
+list = debugger.enumerate_threads()
+
+#リスト中の各スレッドについて各レジスタの値を取得
+for thread in list:
+
+    thread_context = debugger.get_thread_context(thread)
+
+    #レジスタの内容をいくつか取得
+    print "[*] Dumping registers for thread ID: 0x%08x" % thread
+    print "[**] EIP: 0x%08x" % thread_context.Eip
+    print "[**] ESP: 0x%08x" % thread_context.Esp
+    print "[**] EBP: 0x%08x" % thread_context.Ebp
+    print "[**] EAX: 0x%08x" % thread_context.Eax
+    print "[**] EBX: 0x%08x" % thread_context.Ebx
+    print "[**] ECX: 0x%08x" % thread_context.Ecx
+    print "[**] EDX: 0x%08x" % thread_context.Edx
+    print "[*] END DUMP"
+
 debugger.detach()
